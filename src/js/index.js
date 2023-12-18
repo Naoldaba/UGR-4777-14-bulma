67e054382d288c77ca33bb5cee9d858ee49aeb41
NaolDaba <nahafile@gmail.com>
2023-12-18 09:19:58 +0300

    added styles to my portfolio using bulma framework

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const bugerIcon=document.querySelector('#burger')
+const navbarMenu=document.querySelector('#nav-links');
+
+bugerIcon.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('is-active');
+})
